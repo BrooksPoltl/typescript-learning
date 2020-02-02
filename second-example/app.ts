b19d7e81183478382b013279166bd93cbe52cee1
BrooksPoltl <bpoltl1@gmail.com>
2020-02-02 16:15:42 -0600

    never

--- second-example/app.ts
+++ second-example/app.ts
@@ -6,3 +6,10 @@ userInput = 'Brooks';
 if (typeof userInput === 'string') {
   userName = userInput;
 }
+
+function generateError(message: string, code: number): never {
+  throw { message, errorCode: code };
+}
+
+const result = generateError('An error occured!', 500);
+console.log(result);
