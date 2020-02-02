d7edd90293e06691b41dcb5f51f88a30043e70f6
BrooksPoltl <bpoltl1@gmail.com>
2020-02-02 15:31:07 -0600

    enum

--- second-example/app.ts
+++ second-example/app.ts
@@ -1,22 +1,31 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'Brooks',
+//   age: 24,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author']
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR
+}
+const person = {
   name: 'Brooks',
-  age: 24,
+  age: 30,
   hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author']
+  role: Role.ADMIN
 };
 
-person.role[1] = 'admin';
-let favoriteActivities: string[];
-
-favoriteActivities = ['Sports'];
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby);
+if (person.role === Role.AUTHOR) {
+  console.log('is admin');
 }
