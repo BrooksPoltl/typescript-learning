84774d9a1120e1c8522ad877095449d61e959d34
BrooksPoltl <bpoltl1@gmail.com>
2020-02-02 15:25:14 -0600

    tuple

--- second-example/app.ts
+++ second-example/app.ts
@@ -1,9 +1,16 @@
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: 'Brooks',
   age: 24,
-  hobbies: ['Sports', 'Cooking']
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author']
 };
 
+person.role[1] = 'admin';
 let favoriteActivities: string[];
 
 favoriteActivities = ['Sports'];
