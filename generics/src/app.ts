1a6905b3eb50f52d65506ded2015d8e4e0562dee
BrooksPoltl <bpoltl1@gmail.com>
2020-02-05 19:44:32 -0600

    keyof

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -35,3 +35,12 @@ function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
 }
 
 console.log(countAndDescribe(['Hi there!', 'thing']));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return 'Value: ' + obj[key];
+}
+
+extractAndConvert({ name: 'Brooks' }, 'name');
