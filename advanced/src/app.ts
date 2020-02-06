57141d7707f41accb4812a2f7e024266a4e11242
BrooksPoltl <bpoltl1@gmail.com>
2020-02-05 19:04:26 -0600

    function overloads

--- advanced/src/app.ts
+++ advanced/src/app.ts
@@ -21,6 +21,10 @@ type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
 
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: string, b: number): string;
+function add(a: number, b: string): string;
 function add(a: Combinable, b: Combinable) {
   if (typeof a === 'string' || typeof b === 'string') {
     return a.toString() + b.toString();
@@ -112,3 +116,7 @@ const errorBag: ErrorContainer = {
   email: 'Not a valid email!',
   username: 'must start with a capital character'
 };
+
+const result = add('brooks', 'poltl');
+
+result.split(' ');
