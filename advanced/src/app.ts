bbc9e7c41012c00722811d29b07beb4c688d1dae
BrooksPoltl <bpoltl1@gmail.com>
2020-02-05 19:10:16 -0600

    nullish

--- advanced/src/app.ts
+++ advanced/src/app.ts
@@ -123,8 +123,13 @@ result.split(' ');
 
 const fetchedUserData = {
   id: 'u1',
-  name: 'Max'
+  name: 'Max',
   job: { title: 'CEO', description: 'My own company' }
 };
 
 console.log(fetchedUserData?.job?.title);
+
+const userInput = '';
+
+const storedData = userInput ?? 'DEFAULT';
+console.log(storedData);
