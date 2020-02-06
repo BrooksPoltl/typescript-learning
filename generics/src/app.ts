364a178e951971125a065ce2a9f1976ae806ded5
BrooksPoltl <bpoltl1@gmail.com>
2020-02-05 19:35:04 -0600

    working with constraints

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -12,13 +12,10 @@ names[0].split(' ');
 //   data.split(' ');
 // });
 
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
-const mergedObj = merge<{ name: string }, { age: number }>(
-  { name: 'Brooks' },
-  { age: 24 }
-);
+const mergedObj = merge({ name: 'Brooks' }, { age: 24 });
 
-console.log(mergedObj.age);
+console.log(mergedObj.name);
