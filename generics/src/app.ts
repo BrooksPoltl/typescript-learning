3a119316fe150e2cb7c8414fa0d3b6e1f0f9d2ba
BrooksPoltl <bpoltl1@gmail.com>
2020-02-05 19:24:12 -0600

    generic builtins

---
+++ generics/src/app.ts
@@ -0,0 +1,13 @@
+const names: Array<string> = ['brooks', 'manuel'];
+
+names[0].split(' ');
+
+const promise: Promise<string> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('This is done!');
+  }, 2000);
+});
+
+promise.then(data => {
+  data.split(' ');
+});
