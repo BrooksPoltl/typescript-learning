8a70c071a059c84ea7ac1f458c42f7b0e4656cd5
BrooksPoltl <bpoltl1@gmail.com>
2020-02-08 08:53:33 -0600

    setup

--- drag/src/app.ts
+++ drag/src/app.ts
@@ -14,6 +14,7 @@ class projectInput {
       true
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
     this.attach();
   }
   private attach() {
