7f3800c31b710ca522693300e039976678676485
BrooksPoltl <bpoltl1@gmail.com>
2020-02-08 09:31:57 -0600

    todo list working

--- react-demo/src/App.tsx
+++ react-demo/src/App.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
+import { Todo } from './components/todo.model';
 
-const App = () => {
-  return <div>test</div>;
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const todoAddHandler = (text: string) => {
+    setTodos(prevTodos => [
+      ...prevTodos,
+      { text, id: Math.random().toString() }
+    ]);
+  };
+  return (
+    <div>
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} />
+    </div>
+  );
 };
 
 export default App;
