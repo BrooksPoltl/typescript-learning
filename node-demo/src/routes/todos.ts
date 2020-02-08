f3a3e8f3e470b5ae22900b4afb0757bb305d9ea5
BrooksPoltl <bpoltl1@gmail.com>
2020-02-08 10:44:10 -0600

    patched

--- node-demo/src/routes/todos.ts
+++ node-demo/src/routes/todos.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { createTodo } from '../controllers/todos';
+import { createTodo, getTodos, updateTodo } from '../controllers/todos';
 const router = Router();
 
 router.post('/', createTodo);
-router.get('/', () => {});
-router.patch('/:id', () => {});
+router.get('/', getTodos);
+router.patch('/:id', updateTodo);
 router.delete('/:id', () => {});
 
 export default router;
